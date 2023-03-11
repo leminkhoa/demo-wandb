8ab986ebfb750e709056fd2bb1480abe15916b4c
leminkhoa <51240716+leminkhoa@users.noreply.github.com>
2023-03-11 09:05:10 +0700

    Add demo-02, update .gitignore to ignore mlruns, update requirements for numpy version

---
+++ demo-02-mlpipeline-components-in-mlflow/solution/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mlflow run . \
+-P file_url=https://raw.githubusercontent.com/scikit-learn/scikit-learn/4dfdfb4e1bb3719628753a4ece995a1b2fa5312a/sklearn/datasets/data/iris.csv \
+-P artifact_name=iris \
+-P artifact_type=raw_data \
+-P artifact_description='The sklearn IRIS dataset'
